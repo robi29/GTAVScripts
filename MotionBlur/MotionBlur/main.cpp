db10761076430e12804e65fa6a36e16e76161503
robi29 <robinson1994@interia.pl>
2019-04-13 11:17:39 +0200

    Code clean-up. Changed tabs to spaces.

--- MotionBlur/MotionBlur/main.cpp
+++ MotionBlur/MotionBlur/main.cpp
@@ -1,7 +1,7 @@
 /*
-	THIS FILE IS A PART OF GTA V SCRIPT HOOK SDK
-				http://dev-c.com			
-			(C) Alexander Blade 2015
+    THIS FILE IS A PART OF GTA V SCRIPT HOOK SDK
+                http://dev-c.com            
+            (C) Alexander Blade 2015
 */
 
 #include "..\..\inc\main.h"
@@ -10,17 +10,17 @@
 
 BOOL APIENTRY DllMain(HMODULE hInstance, DWORD reason, LPVOID lpReserved)
 {
-	switch (reason)
-	{
-	case DLL_PROCESS_ATTACH:
-		scriptRegister(hInstance, ScriptMain);
-		keyboardHandlerRegister(OnKeyboardMessage);
-		break;
-	case DLL_PROCESS_DETACH:
-		UnloadScript();
-		scriptUnregister(hInstance);
-		keyboardHandlerUnregister(OnKeyboardMessage);
-		break;
-	}		
-	return TRUE;
+    switch (reason)
+    {
+    case DLL_PROCESS_ATTACH:
+        scriptRegister(hInstance, ScriptMain);
+        keyboardHandlerRegister(OnKeyboardMessage);
+        break;
+    case DLL_PROCESS_DETACH:
+        UnloadScript();
+        scriptUnregister(hInstance);
+        keyboardHandlerUnregister(OnKeyboardMessage);
+        break;
+    }       
+    return TRUE;
 }
